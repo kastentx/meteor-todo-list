b1f0349079b70a2caa45a010bd84c3967dce49ca
Nick Kasten <kastencode@gmail.com>
2016-09-12 01:08:11 -0500

    attempting to add checkbox and delete button

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,10 +1,40 @@
 import React, { Component, PropTypes } from 'react';
 
+import { Tasks } from '../api/tasks.js';
+
 // Task component - represents a single to-do item
 export default class Task extends Component {
+	toggleChecked() {
+		// Set the checked property to the opposite of its current value
+		Tasks.update(this.props.task._id, {
+			$set: { checked: !this.props.task.checked },
+		});
+	}
+
+	deleteThisTask() {
+		Tasks.remove(this.props.task._id);
+	}
+
 	render() {
+		// give tasks a different className when they are checked off
+		// so we can style them differently in CSS
+		const taskClassName = this.props.task.checked ? 'checked' : '';
+
 		return (
-			<li>{this.props.task.text}</li>
+			<li className={taskClassName}>
+				<button className="delete" onClick={this.deleteThisTask.bind(this)}>
+					&times;
+				</button>
+
+				<input 
+					type="checkbox"
+					readOnly
+					checked={this.props.task.checked}
+					onClick={this.props.toggleChecked.bind(this)}
+				/>
+
+				<span className="text">{this.props.task.text}</span>
+			</li>
 		);
 	}
 }
