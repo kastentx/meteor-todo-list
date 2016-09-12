77a99b593a627cf4615de890612b6afc52b529a2
Nick Kasten <kastencode@gmail.com>
2016-09-11 23:52:09 -0500

    new task submissions work!

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
 import { createContainer } from 'meteor/react-meteor-data';
 
 import { Tasks } from '../api/tasks.js';
@@ -7,17 +8,33 @@ import Task from './Task.jsx';
 
 // App component - represents the whole app
 class App extends Component {
+	handleSubmit(event) {
+		event.preventDefault();
+
+		// Find the text field via the React ref
+		const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+		Tasks.insert({
+			text,
+			createdAt: new Date(), // current time 
+		});
+
+		// clear form
+		ReactDOM.findDOMNode(this.refs.textInput).value = '';
+	}
+
 	renderTasks() {
 		return this.props.tasks.map((task) => ( 
 			<Task key={task._id} task={task} />
 		));
 	}
+
 	render() {
 		return (
 			<div className="container">
+
 				<header>
 					<h1>To-Do List</h1>
-
 					<form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
 						<input
 							type="text"
@@ -25,13 +42,12 @@ class App extends Component {
 							placeholder="Type to add new tasks"
 						/>
 					</form>
-
-
 				</header>
-				
+		
 				<ul>
 				{ this.renderTasks() }
 				</ul>		
+
 			</div>
 		); 
 	}
