a0904e6da839884b1691785a1552d1f256904a30
Nick Kasten <kastencode@gmail.com>
2016-09-16 16:51:10 -0500

    created first unit test canDeleteOwnedTask

---
+++ imports/api/tasks.tests.js
@@ -0,0 +1,39 @@
+/* eslint-env mocha */
+import { Meteor } from 'meteor/meteor';
+import { Random } from 'meteor/random';
+import { assert } from 'meteor/practicalmeteor:chai';
+
+import { Tasks } from './tasks.js';
+
+if (Meteor.isServer) {
+	describe('Tasks', () => {
+		describe('methods', () => {
+			const userId = Random.id();
+			let taskId;
+
+			beforeEach(() => {
+				Tasks.remove({});
+				taskId = Tasks.insert({
+					text: 'test text',
+					createdAt: new Date(),
+					owner: userId,
+					username: 'tmeasday'
+				});
+			}); 
+
+			it('can delete owned task', () => {
+				// find the implementation of the task method so we can test it in isolation
+				const deleteTask = Meteor.server.method_handlers['tasks.remove'];
+
+				// set up a fake method invocation that looks like what the method expects
+				const invocation = { userId };
+
+				// run the method with `this` set to the fake invocation
+				deleteTask.apply(invocation, [taskId]);
+
+				// Verify that the method does what we expected
+				assert.equal(Tasks.find().count(), 0);
+			});
+		});
+	});
+}
