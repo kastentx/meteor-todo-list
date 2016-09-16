d71040d20d1a7bc764acdd705308a090ebbb9ece
Nick Kasten <kastencode@gmail.com>
2016-09-16 12:31:16 -0500

    removed autopublish... replaced with Meteor publish/subscribe

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -1,3 +1,16 @@
 import { Mongo } from 'meteor/mongo';
 
 export const Tasks = new Mongo.Collection('tasks');
+
+if (Meteor.isServer) {
+	// this code only runs on the server
+	Meteor.publish('tasks', function tasksPublication() {
+		return Tasks.find();
+	});
+}
+
+Meteor.methods({
+	'tasks.insert'(text) {
+		check(text, String);
+	}
+})
--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -97,6 +97,8 @@ App.propTypes = {
 };
 
 export default createContainer(() => {
+	Meteor.subscribe('tasks');
+
 	return {
 		tasks : Tasks.find({}, { sort: { createdAt : -1 } }).fetch(),	
 		incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
