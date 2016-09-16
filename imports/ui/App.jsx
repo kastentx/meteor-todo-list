5104cea04091210ebbddd62b5588bd4e07e140f6
Nick Kasten <kastencode@gmail.com>
2016-09-16 01:34:18 -0500

    number of incomplete tasks is displayed in title

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -52,7 +52,7 @@ class App extends Component {
 			<div className="container">
 
 				<header>
-					<h1>To-Do List</h1>
+					<h1>Todo List ({this.props.incompleteCount})</h1>
 
 					<label className="hide-completed">
 						<input
@@ -84,10 +84,12 @@ class App extends Component {
 
 App.propTypes = {
 	tasks: PropTypes.array.isRequired,
+	incompleteCount: PropTypes.number.isRequired,
 };
 
 export default createContainer(() => {
 	return {
 		tasks : Tasks.find({}, { sort: { createdAt : -1 } }).fetch(),	
+		incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
 	};
 }, App);
