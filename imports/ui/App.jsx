df794157589d5f59b1f540635e14e06b94b9e263
Nick Kasten <kastencode@gmail.com>
2016-09-16 02:37:46 -0500

    users are only able to post new items while logged in

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
+import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
 
 import { Tasks } from '../api/tasks.js';
@@ -25,7 +26,9 @@ class App extends Component {
 
 		Tasks.insert({
 			text,
-			createdAt: new Date(), // current time 
+			createdAt: new Date(),	// current time 
+			owner: Meteor.userId(), // _id of logged in user
+			username: Meteor.user().username, // username of logged in user
 		});
 
 		// clear form
@@ -67,13 +70,15 @@ class App extends Component {
 
 					<AccountsUIWrapper />
 
-					<form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
-						<input
-							type="text"
-							ref="textInput"
-							placeholder="Type to add new tasks"
-						/>
-					</form>
+					{ this.props.currentUser ?
+						<form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
+							<input
+								type="text"
+								ref="textInput"
+								placeholder="Type to add new tasks"
+							/>
+						</form> : ''
+					}
 				</header>
 		
 				<ul>
@@ -88,11 +93,13 @@ class App extends Component {
 App.propTypes = {
 	tasks: PropTypes.array.isRequired,
 	incompleteCount: PropTypes.number.isRequired,
+	currentUser: PropTypes.object,
 };
 
 export default createContainer(() => {
 	return {
 		tasks : Tasks.find({}, { sort: { createdAt : -1 } }).fetch(),	
 		incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
+		currentUser: Meteor.user(),
 	};
 }, App);
